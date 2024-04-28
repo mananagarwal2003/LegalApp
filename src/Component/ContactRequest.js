d26035c555e888075ceffa23dea3b1743b5edba2
Khushi740 <99756558+Piyush9140@users.noreply.github.com>
2024-04-29 04:26:13 +0530

    Accept request feature

--- src/Component/ContactRequest.js
+++ src/Component/ContactRequest.js
@@ -1,37 +1,40 @@
 import { StyleSheet, Text, View, Pressable, Image } from "react-native";
 import React, { useContext } from "react";
-// import { UserType } from "../UserContext";
 import { useNavigation } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { api } from "../api/api";
+// import { UserType } from "../Context/UserContext";
 
 const ContactRequest = ({ item, friendRequests, setFriendRequests }) => {
 //   const { userId, setUserId } = useContext(UserType);
   const navigation = useNavigation();
-//   const acceptRequest = async (friendRequestId) => {
-//     try {
-//       const response = await fetch(
-//         "http://localhost:8000/friend-request/accept",
-//         {
-//           method: "POST",
-//           headers: {
-//             "Content-Type": "application/json",
-//           },
-//           body: JSON.stringify({
-//             senderId: friendRequestId,
-//             recepientId: userId,
-//           }),
-//         }
-//       );
+  const acceptRequest = async (friendRequestId) => {
+    try {
+        const LawyerID = await AsyncStorage.getItem("LawyerID");
+      const response = await fetch(
+        `${api}/friend-request/accept`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            senderId: friendRequestId,
+            recepientId: LawyerID,
+          }),
+        }
+      );
 
-//       if (response.ok) {
-//         setFriendRequests(
-//           friendRequests.filter((request) => request._id !== friendRequestId)
-//         );
-//         navigation.navigate("Chats");
-//       }
-//     } catch (err) {
-//       console.log("error acceptin the friend request", err);
-//     }
-//   };
+      if (response.ok) {
+        setFriendRequests(
+          friendRequests.filter((request) => request._id !== friendRequestId)
+        );
+        navigation.navigate("Bookings");
+      }
+    } catch (err) {
+      console.log("error acceptin the friend request", err);
+    }
+  };
   return (
     <Pressable
       style={{
@@ -52,7 +55,7 @@ const ContactRequest = ({ item, friendRequests, setFriendRequests }) => {
       </Text>
 
       <Pressable
-        onPress={() => {}}
+        onPress={() => acceptRequest(item._id)}
         style={{ backgroundColor: "#0066b2", padding: 10}}
       >
         <Text style={{ textAlign: "center", color: "white" }}>Accept</Text>
